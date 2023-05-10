7d90ea67ba2b54add85d6859b76ef0f286176076
taniamwnsa <114711980+taniamwnsa@users.noreply.github.com>
2023-05-09 23:13:07 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+ document.querySelector('.menu-btm').addEventListener('click',()=>{
+    document.querySelector('.nav-menu').classList.toggle('show')});
+
+    ScrollReveal().reveal('.showcase');
+    ScrollReveal().reveal('.news-cards',{delay: 500 });
+    ScrollReveal().reveal('.cards-banner-one',{delay: 500 });
